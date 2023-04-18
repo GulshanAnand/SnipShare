1f4cd6f2eb0ff2fb3998d14237156e9bee030457
Gulshan Anand <gulshan2052@gmail.com>
2023-04-18 17:31:23 +0530

    production ready

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+const path = require('path');
 
 const dotenv = require('dotenv');
 dotenv.config();
@@ -8,17 +9,21 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static('build'));
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 const snipRouter = require("./routes/snip.js");
 const userRouter = require("./routes/user.js");
 
-app.get("/", (req, res) => {
-    res.send("hello world");
-});
+// app.get("/", (req, res) => {
+//     res.send("hello world");
+// });
 
 app.use("/snip", snipRouter);
 app.use("/auth", userRouter);
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
 
 mongoose.connect(process.env.MONGO_URL + "/snipDB")
     .then(() => console.log('Connected Successfully'));
